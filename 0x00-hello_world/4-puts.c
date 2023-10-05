7fe6d6fcd8d979f8bbcc04b9786471526f80124b
helsinki <taha.chad2001@gmail.com>
2023-10-05 09:53:48 -0700

    auto commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,17 +2,14 @@
 
 
 /**
+ *main - Entry point
  *
- *Description :prints Programming is like building a multilingual puzzle
+ *Description : prints "Programming is like building a multilingual puzzle"
  *
- *
- *
- *
+ *Return alwaas 0 (for success)
  */
 int main(void) 
 {
-
 puts("\"Programming is like building a multilingual puzzle");
-
 return (0);
 }
