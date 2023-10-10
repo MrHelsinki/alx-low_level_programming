4eb5a3226f9fbf034a0fb0f8ef63727e08b9b087
MrHelsinki <taha.chad2001@gmail.com>
2023-10-10 17:37:35 +0100

    automatic push script

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * main - prints alphabet
+  *
+  * Return: 0 on Success
+  */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet()
+{
+	char i;
+	while ( i < "z")
+	{
+		_putchar(i);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
+int print_alphabet(void);
+
 
 #endif
