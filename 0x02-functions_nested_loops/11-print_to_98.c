fcc64be0cb4892f117c3cb2dfc3a939fc1fb3c67
MrHelsinki <taha.chad2001@gmail.com>
2023-10-11 00:00:26 +0100

    automatic push script

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 
 /**
   * print_to_98 - print to 98 from n
-  * @n: int var to print from 
+  * @n: int var to print from
   */
 void print_to_98(int n)
 {
@@ -12,18 +12,24 @@ void print_to_98(int n)
 		for (n; n <= 98; n++)
 		{
 			if (n != 98)
-				printf("%d, ", n);
+				_putchar('n' + '0');
+				_putchar(',');
+				_putchar(' ');
 			else if (n == 98)
-				printf("%d\n", n);
+				_putchar('n' + '0')
+				_putchar('\n');
 		}
 	} else if (n >= 98)
 	{
 		for (n; n >= 98; n--)
 		{
 			if (n != 98)
-				printf("%d, ", n);
+				_putchar('n' + '0');
+				_putchar(',');
+				_putchar('\n');
 			else if (n == 98)
-				printf("%d\n", n);
+				_putchar('n' + '0');
+				_putchar('\n');
 		}
 	}
 }
