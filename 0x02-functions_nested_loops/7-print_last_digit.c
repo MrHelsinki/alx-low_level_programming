91717ead64a416c8aee6e17f1be0aaf4af4f18f5
MrHelsinki <taha.chad2001@gmail.com>
2023-10-10 21:04:32 +0100

    automatic push script

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * print_last_digit - prints last digit of number
+  *
+  * @n: number being operated on
+  *
+  * Return: last digit of said number
+  */
+int print_last_digit(int n)
+{
+	int abs_n;
+
+	int nil;
+
+	if (n < 0)
+		abs_n = -n;
+	else
+		abs_n = n;
+
+	while (abs_n > 9)
+	{
+		abs_n /= 10;
+		nil++;
+	}
+
+	return (n[nil]);
+}
