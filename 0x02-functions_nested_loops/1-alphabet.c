07b4bb66e516188df896995ddf132f6d4e21697f
MrHelsinki <taha.chad2001@gmail.com>
2023-10-10 14:39:09 +0100

    automatic push script

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * main - prints alphabet
+  *
+  * Return: 0 on  Success
+  */
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n"
+	int alphLen = 27;
+	int i;
+
+	for (i = 0; i < alphLen; i++)
+	{
+		printChar(alphabet[i]);
+	}
+	return (0);
+}
