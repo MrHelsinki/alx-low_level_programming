391d1c5ddf0881df064d6c0e1bd3c39284cf7e24
MrHelsinki <taha.chad2001@gmail.com>
2023-10-10 22:04:37 +0100

    automatic push script

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,9 +11,16 @@ int print_last_digit(int n)
 {
 	int x;
 
+	if (n == INT_MIN)
+	{
+		x = 8;
+		_putchar(x '0');
+		return (x);
+	}
+
 	if (n < 0)
 		n = -n;
-	
+
 	x = n % 10;
 
 	_putchar(x + '0');
