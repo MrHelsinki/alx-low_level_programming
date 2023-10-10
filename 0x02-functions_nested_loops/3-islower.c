99f7d1b199289fe4cdf85f788a7ee42f411ce6fa
MrHelsinki <taha.chad2001@gmail.com>
2023-10-10 19:37:21 +0100

    automatic push script

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,14 @@
 #include "main.h"
 
 /**
-  *
   * _islower - print 1 if lower case ; 0 if else
   *
-  * Return - 1 if lower case - 0 if else 
-  *
-  *
+  * Return - 1 if lower case - 0 if else
   */
 
 int _islower(int c)
 {
-	int asciiValue = (int) c;
-
-	if (asciiValue < 97 && asciiValue > 122)
+	if (av < 97 && av > 122)
 		return (1);
 	else
 		return (0);
