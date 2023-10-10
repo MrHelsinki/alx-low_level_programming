cee2b23243a78053683078162ef6b25237531b5c
MrHelsinki <taha.chad2001@gmail.com>
2023-10-10 20:31:41 +0100

    automatic push script

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * print_sign - prints sign of number
+  *
+  * @n: number tested
+  *
+  * Return: 1 if positive or -1 if negative or 0 if equal 0
+  */
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putcahr('+');
+		return (1);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+}
