b50882fba11164009314cad1d88170a05e1db2af
MrHelsinki <taha.chad2001@gmail.com>
2023-10-13 00:11:08 +0100

    automatic push script

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
-  * print_number - prints num (the hard way)
-  * @n: num
-  */
-int print_number(int n)
+ * print_number - prints an integer
+ * @n: integer to be printed
+ */
+void print_number(int n)
 {
-	unsigned int i;
+	unsigned int n1;
 
 	if (n < 0)
 	{
-		i = -n;
+		n1 = -n;
 		_putchar('-');
 	} else
 	{
-		i = n;
+		n1 = n;
 	}
 
-	if (i / 10)
+	if (n1 / 10)
 	{
-		print_number(i / 10);
+		print_number(n1 / 10);
 	}
 
-	_putchar((i % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
