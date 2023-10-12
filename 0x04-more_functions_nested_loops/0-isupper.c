f99b4d06cb9cca79210941834e96713f0df58472
MrHelsinki <taha.chad2001@gmail.com>
2023-10-12 18:57:52 +0100

    automatic push script

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * _isupper - check uppercase
+  * @c: letter being checked
+  * Return: 1 if uppercase 0 if not
+  */
+int _isupper(int c)
+{
+	if (c > 65 && c < 90)
+		return (1);
+
+	return (0);
+}
