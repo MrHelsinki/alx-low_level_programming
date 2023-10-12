dfdb192e2f0310973ffc6a9d752404b5148203d6
MrHelsinki <taha.chad2001@gmail.com>
2023-10-12 01:42:26 +0100

    automatic push script

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* largest_number - determine largest number
+* @a: first num
+* @b: second num
+* @c: third num
+
+* Return: result
+*/
+
+int largest_number(int a, int b, int c)
+{
+	int lrg;
+
+	if (a > b && a > c)
+	{
+		lrg = a;
+	}
+	else if (a > b && c > a)
+	{
+		lrg = c;
+	}
+	else if (b > c)
+	{
+		lrg = b;
+	}
+	else
+	{
+		lrg = c;
+	}
+
+return (lrg);
+}
