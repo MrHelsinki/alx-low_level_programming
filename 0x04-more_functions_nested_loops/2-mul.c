2ccb42524ee48f6ef127fe4754d7032e5ff6b0fa
MrHelsinki <taha.chad2001@gmail.com>
2023-10-12 19:16:11 +0100

    automatic push script

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * mul - multiplier
+  * @a: int 1
+  * @b: int 2
+  * Return: result
+  */
+int mul(int a, int b)
+{
+	int multi;
+
+	multi = a * b;
+
+	return (multi);
+}
