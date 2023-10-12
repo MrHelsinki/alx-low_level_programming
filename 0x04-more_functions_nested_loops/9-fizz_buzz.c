43253ffc580596ba901b1e8d3e99f951bf616a20
MrHelsinki <taha.chad2001@gmail.com>
2023-10-12 22:52:20 +0100

    automatic push script

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - fizz buzz
+  * Return: 0 on success
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if
+			printf("Buzz");
+		else
+			print("%d", i)
+	}
+
+	printf("\n")
+
+}
