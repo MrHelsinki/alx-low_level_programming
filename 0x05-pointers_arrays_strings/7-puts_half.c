956c8164a23db784d2a29951e317551f44cc9f74
MrHelsinki <taha.chad2001@gmail.com>
2023-10-16 21:01:24 +0100

    auto push bash script

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  * puts_half - func
+  * @str: par
+  */
+void puts_half(char *str)
+{
+
+	int len;
+
+	char *tmp = str;
+
+	len = 0;
+	while (*tmp)
+	{
+		len++;
+		tmp++;
+	}
+
+	int halflen;
+
+	halflen = len / 2;
+
+	int i;
+
+	str += halflen;
+
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	
+}
