4868b3df856911319e629fe220ba7d041a8b4d87
MrHelsinki <taha.chad2001@gmail.com>
2023-10-16 21:55:54 +0100

    auto push bash script

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * _strcpy - func
+  * @dest: par 1
+  * @dest: par 2
+  * Return: pointer
+  */
+void *_strcpy(char *dest, char *src)
+{
+	char *destination = dest;
+
+	while (*src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	*dest = '\0';
+
+	return (destination);
+}
