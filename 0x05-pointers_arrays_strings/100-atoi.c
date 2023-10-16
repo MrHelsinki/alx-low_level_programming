3f75487695e21dab24f478c7ee5d3bf735da79ef
MrHelsinki <taha.chad2001@gmail.com>
2023-10-17 00:59:16 +0100

    auto push bash script

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,57 +1,37 @@
 #include "main.h"
-#include <stddef.h>
-
 /**
-  *
-  *
-  *
-  */
+ * _atoi - func
+ * @s: par
+ * Return: result
+ */
 int _atoi(char *s)
 {
+	short boolean;
+
 	int i;
-	char x;
-	int n;
-	int revnum;
-	int loop;
-	int signNumber;
-	int count;
-	int tmpRevNum;
 
-	signNumber = 1;
-	n = NULL
-	revnum = 0;
-	loop = 1;
-	i = 0;
-	count = 0;
+	int minus;
 
-	while (*s)
+	int result;
+
+	i = minus = result = boolean = 0;
+	minus = -1;
+
+	while (s[i] != '\0')
 	{
-		x = s[i];
-		i++;
+		if (s[i] == '-')
+			minus *= -1;
 
-		if (x >= '0' && x <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			n = x - '0';
-
-			revnum += n * loop;
-			loop = loop * 10;
-
-		} else if (x == '-')
-		{
-				signNumber = signNumber * -1;
-		} else
-		{
-			break;
+			result *= 10;
+			result -= (s[i] - '0');
+			boolean = 1;
 		}
+		else if (boolean == 1)
+			break;
+		i++;
 	}
-	tmpRevNum = revnum;
-	while (tmpRevNum != 0)
-	{
-		revnum /= 10;
-		count++;
-	}
-	while (count != 0)
-	{
-		
-	}
+	result *= minus;
+	return (result);
 }
