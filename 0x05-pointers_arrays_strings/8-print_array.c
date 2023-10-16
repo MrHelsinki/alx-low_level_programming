d891d814b148d661987f6571fd5a820be164103b
MrHelsinki <taha.chad2001@gmail.com>
2023-10-16 21:26:40 +0100

    auto push bash script

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,12 +10,16 @@ void print_array(int *a, int n)
 
 	int i;
 
-	i = 0;
-	while (i < n)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d, ",*(a+i));
-		i++;
+		if (i == n - 1)
+		{
+			printf("%d", a[i]);
+		} else
+		{
+			printf("%d, ", a[i]);
+		}
 	}
 
-	_putchar('\n');
+	printf("\n");
 }
