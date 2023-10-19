1b23896e20064a8dcfd9bbb3d30c5a9b5e582d3c
MrHelsinki <taha.chad2001@gmail.com>
2023-10-19 18:39:41 +0100

    auto push bash script

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  *_stncpy - func to copy str 2 to str 1 adress
+  *_strncpy - func to copy str 2 to str 1 adress
   *@dest: dest adress parameter
   *@src: src adress parameter
   *@n: num of bytes to cpy
@@ -14,13 +14,17 @@ char *_strncpy(char *dest, char *src, int n)
 
 	i = 0;
 
-	while(i < n && src[i] != '\0')
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
 
-	dest[i] = '\0';
+	while (i < n)
+	{
+		dest[i] = '0';
+		i++;
+	}
 
 	return (dest);
 }
