56895b15b42bcd72e1b4f4ecef3400dd8998ea21
MrHelsinki <taha.chad2001@gmail.com>
2023-10-23 23:55:27 +0100

    auto push bash script

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,17 @@
 
 char *_strchr(char *s, char c)
 {
-
-	while (*s)
+	while (*s != '\0')
 	{
 		if (*s == c)
+		{
 			return (s);
+		}
 		s++;
 	}
-
-	return (NULL);
+	if (*s == c)
+	{
+		return (s);
+	}
+return (NULL);
 }
