f31631be3503e15e7ccdee504d3e7aa1a1fa98eb
MrHelsinki <taha.chad2001@gmail.com>
2023-10-25 10:00:00 +0100

    auto push bash script

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,14 +3,14 @@
 /**
   *factorial - fuc
   *@n: par
-  *return - int
+  *Return: return
   */
 int factorial(int n)
 {
 	if (n < 0)
 	{
 		return (-1);
-	} else if ( n <= 1)
+	} else if (n <= 1)
 	{
 		return (1);
 	} else
