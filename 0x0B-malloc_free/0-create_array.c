54ed2a3d91733148a4ed73380db407f24fb8d559
MrHelsinki <taha.chad2001@gmail.com>
2023-10-30 08:39:00 +0100

    auto push bash script

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+  *create_array - func
+  *@size: par
+  *@c: par
+  *Return: ptr
+  */
+char *create_array(unsigned int size, char c)
+{
+	int *ptr;
+
+	if (size == 0)
+		return (NULL);
+
+	ptr = (char *) malloc(size * sizeof(char));
+
+	if (*ptr == NULL)
+		return (NULL);
+
+	ptr[0] = c;
+
+	return (ptr);
+}
