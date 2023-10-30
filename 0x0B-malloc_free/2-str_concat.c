1934c5f3b3c68c42bfe2dd880019956b8a35f337
MrHelsinki <taha.chad2001@gmail.com>
2023-10-30 09:58:58 +0100

    auto push bash script

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+  *str_concat - func
+  *@s1: par
+  *@s2: par
+  *Return: char *
+  */
+
+char *str_concat(char *s1, char *s2)
+{
+	int len1, len2, i, y;
+
+	char *ptr;
+
+	len1 = 0;
+	len2 = 0;
+	i = 0;
+	y = 0;
+
+	while (s1[len1])
+		len1++;
+
+	while (s2[len2])
+		len2++;
+
+	ptr = malloc((len1 + len2 + 1) * sizeof(char));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	while (s1[i])
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+
+	while (s2[y])
+	{
+		ptr[i] = s2[y];
+		i++;
+		y++;
+	}
+
+	ptr[i] = '\0';
+
+	return (ptr);
+}
+
