852c965aa5137a4959c539340bfa8bf80ef0aaae
MrHelsinki <taha.chad2001@gmail.com>
2023-10-31 06:54:11 +0100

    auto push bash script

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,74 +1,50 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
 
 /**
-  *str_concat - func
-  *@s1: par
-  *@s2: par
-  *Return: char *
-  */
-
-int *_strlen(char *str)
-{
-	int *ptr;
-
-	int len;
-
-	len = 0;
-
-	if (str != NULL)
-	{
-		while (str[len])
-		{
-			len ++;
-		}
-	} else
-	{
-		return (NULL);
-	}
-
-	ptr = &len;
-
-	return (ptr);
-}
-
+ * *str_concat - func
+ * @s1: par
+ * @s2: par
+ * Return: return
+ */
 char *str_concat(char *s1, char *s2)
 {
-	int *len1, *len2;
-		
-	int i, y;
-
 	char *ptr;
 
+	int i, j, len1, len2;
+
+	i = 0;
+	j = 0;
 	len1 = 0;
 	len2 = 0;
-	i = 0;
-	y = 0;
-
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	ptr = malloc((*len1 + *len2 + 1) * sizeof(char));
-	if (ptr == NULL)
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
 		return (NULL);
-	if (s1 != NULL)
+	i = 0;
+	j = 0;
+	if (s1)
 	{
-		while (s1[i])
+		while (i < len1)
 		{
-			ptr[i] = s1[i];
+			s3[i] = s1[i];
 			i++;
 		}
 	}
-	if (s2 != NULL)
-	{
-		while (s2[y])
-		{
-			ptr[i] = s2[y];
-			i++;
-			y++;
-		}
-	}
-	ptr[i] = '\0';
-	return (ptr);
-}
 
+	if (s2)
+	{
+		while (i < (len1 + len2))
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
+		}
+	}
+
+	s3[i] = '\0';
+	return (s3);
+}
