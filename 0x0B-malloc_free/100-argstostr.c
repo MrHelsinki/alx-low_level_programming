7abf6919d1adf50821819177ee284c64f5474081
MrHelsinki <taha.chad2001@gmail.com>
2023-10-31 16:43:58 +0100

    auto push bash script

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,15 +8,10 @@
   *@av:par
   *Return: ptr to char
   */
-
-
-
 char *argstostr(int ac, char **av)
 {
 	int i, y, j, k, l;
-
 	int chars;
-
 	char *ptr;
 
 	(void) ac;
@@ -24,38 +19,29 @@ char *argstostr(int ac, char **av)
 	y = 0;
 	chars = 0;
 	l = 0;
-
 	if (ac == 0 || av == NULL)
 		return (NULL);
-
-	for(j = 0; j < ac; j++)
+	for (j = 0; j < ac; j++)
 	{
-		for(k = 0; av[j][k] != '\0'; k++)
+		for (k = 0; av[j][k] != '\0'; k++)
 		{
 			chars++;
 		}
 	}
-
 	ptr = (char *) malloc(sizeof(char) * (chars + (ac)));
-
 	if (ptr == NULL)
 		return (NULL);
-
-	 for (i = 0; i < ac; i++)
-	 {
-		while(av[i][l])
+	for (i = 0; i < ac; i++)
+	{
+		while (av[i][l])
 		{
 			ptr[y] = av[i][l];
 			l++;
 			y++;
-
 		}
-		if (i < ac - 1)
-		{
-			ptr[y] = '\n';
-			y++;
-		}
+		ptr[y] = '\n';
+		y++;
 		l = 0;
-	 }
-	 return (ptr);
+	}
+	return (ptr);
 }
