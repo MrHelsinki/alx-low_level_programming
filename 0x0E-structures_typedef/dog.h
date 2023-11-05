6a6744c6aed251ede5602cf3d12753b9825b978d
MrHelsinki <taha.chad2001@gmail.com>
2023-11-05 11:51:09 +0100

    auto push bash script

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  *struct dog - dog's struc
+  *@name: name
+  *@age: age
+  *@owner: owner
+  *Description: a dog's info
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
