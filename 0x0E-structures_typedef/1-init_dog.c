fc40510c48dfddf8e878a733b0b8517b22adb348
MrHelsinki <taha.chad2001@gmail.com>
2023-11-05 11:58:06 +0100

    auto push bash script

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+  *init_dog - func
+  *@d: par
+  *@name: par
+  *@age: par
+  *@owner: par
+  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
