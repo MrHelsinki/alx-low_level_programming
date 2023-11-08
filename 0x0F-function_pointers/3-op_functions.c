3fa0d49fb2106f6d4c670251675a98be36b2f83b
MrHelsinki <taha.chad2001@gmail.com>
2023-11-08 21:23:53 +0100

    auto push bash script

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,78 @@
+/**
+  *op_add - add 2 integers
+  *@a: num 1
+  *@b: num 2
+  *Return: result
+  */
+int op_add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
+
+/**
+  *op_sub - sub int 1 from 2
+  *@a: num 1
+  *@b: num 2
+  *Return: result
+  */
+
+int op_sub(int a, int b)
+{
+	int result;
+
+	result = a - b;
+
+	return (result);
+}
+
+/**
+  *op_mul - mul int 1 by 2
+  *@a: num 1
+  *@b: num 2
+  *Return: result
+  */
+
+int op_mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
+
+/**
+  *op_div - div num 1 by 2
+  *@a: num 1
+  *@b: num 2
+  *Return: result
+  */
+
+int op_div(int a, int b)
+{
+	int result;
+
+	result = a / b;
+
+	return (result);
+}
+
+/**
+  *op_mod - modulo num 1 by 2
+  *@a: num 1
+  *@b: num 2
+  *Return: return
+  */
+
+int op_mod(int a, int b)
+{
+	int result;
+
+	result = a % b;
+
+	return (result);
+}
