2813c027c52f589180f7be4b429a44df507c0488
MrHelsinki <taha.chad2001@gmail.com>
2023-11-09 19:55:20 +0100

    auto push bash script

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,19 +9,22 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-
+	char *str;
 	va_list args;
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
+		str = va_arg(args, char *);
+		if (!str)
+			str = "(nil)";
 		if (!separator)
-			printf("%s", va_arg(args, char *));
+			printf("%s", str);
 		else if (separator && i == 0)
-			printf("%s", va_arg(args, char *));
+			printf("%s", str);
 		else
-			printf("%s%s", separator, va_arg(args, char *));
+			printf("%s%s", separator, str);
 	}
 
 	va_end(args);
