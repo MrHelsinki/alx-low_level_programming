b75a4d6680194b12caa95dd63e3ba94b52d958b5
MrHelsinki <taha.chad2001@gmail.com>
2023-11-11 21:04:04 +0100

    auto push script

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+*print_list - prints elems of a list
+*@h: ptr to list
+*Return: Number of nodes printed
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t np = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)");
+		printf("[%u] %s\n", h->len, h->str)
+		np++;
+		h = h->next;
+	}
+
+	return (np);
+}
