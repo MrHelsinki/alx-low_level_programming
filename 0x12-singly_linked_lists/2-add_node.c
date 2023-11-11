5b50d4946d4613e6afb9feb0127768a6c343b373
MrHelsinki <taha.chad2001@gmail.com>
2023-11-11 21:55:47 +0100

    auto push script

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+*add_node - add node at head
+*@head: first node
+*@str: value of the node
+*Return: adress of new elem
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newNode;
+	int len;
+
+	while (str[len])
+		len++;
+	
+	newNode = malloc(sizeof(list_t));
+
+	if (!newNode)
+		return (NULL);
+	
+	newNode->str = strdup(str);
+	newNode->len = len;
+	newNode->next = *head;
+
+	return (newNode);
+
+
+}
