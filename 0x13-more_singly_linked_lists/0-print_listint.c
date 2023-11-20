0eb55e1b9765d96369b8c2d12821bc0d1b17f2ed
MrHelsinki <taha.chad2001@gmail.com>
2023-11-20 03:47:59 +0100

    auto push script

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,9 +11,9 @@ size_t print_listint(const listint_t *h)
 	size_t size = 0;
 	const listint_t *tmp = h;
 	
-	while (tmp->n != NULL)
+	while (tmp != NULL)
 	{
-		printf("%d", tmp->n);
+		printf("%d\n", tmp->n);
 		tmp = tmp->next;
 		size++;
 	}
