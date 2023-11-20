1b0811828c970224cac62553f15fd9ce2373daa9
MrHelsinki <taha.chad2001@gmail.com>
2023-11-20 06:14:33 +0100

    auto push script

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - func 
+ * @head: ptr head
+ * Return: ptr
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
