b5eca23128e2d19b4279472dcf9a555133fca58e
MrHelsinki <taha.chad2001@gmail.com>
2023-11-20 06:07:56 +0100

    auto push script

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,7 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		*head = next;
 		return (1);
 	}
-	current = *head;
+	workingNode = *head;
 	for (i = 0; i < index - 1; i++)
 	{
 		if (workingNode->next == NULL)
