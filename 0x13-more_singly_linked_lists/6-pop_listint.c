f723868b975b4ee9ba92badb9b80451f54143e17
MrHelsinki <taha.chad2001@gmail.com>
2023-11-20 05:45:00 +0100

    auto push script

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - del first elem and return value
+ * @head: head ptr
+ * Return: int
+ **/
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int value;
+
+	if (*head == NULL)
+		return (0);
+
+	tmp = *head;
+	*head = tmp->next;
+	value = tmp->n;
+	free(tmp);
+	return (tmp);
+}
