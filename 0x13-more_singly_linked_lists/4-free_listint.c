8eabc1d3c351a591433e7c98faf1bf3a9bf24fcd
MrHelsinki <taha.chad2001@gmail.com>
2023-11-20 05:21:48 +0100

    auto push script

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+*free_listint - free
+*@head: ptr head node
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t *tmp = head;
+
+	while (tmp->next != NULL)
+	{
+		free(head);
+		head = tmp->next;
+		tmp = tmp->next;
+	}
+
+	free(head);
+}
+
