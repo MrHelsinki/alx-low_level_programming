8c2e18ecddd71f7d730b4d04e92ce74cc62f191a
MrHelsinki <taha.chad2001@gmail.com>
2023-11-20 05:04:25 +0100

    auto push script

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,14 +12,21 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *tmp = *head;
 	listint_t *new;
 
-	while (tmp->next != NULL)
-		tmp = tmp->next;
-
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
+
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
 	tmp->next = new;
 
 	return (new);
