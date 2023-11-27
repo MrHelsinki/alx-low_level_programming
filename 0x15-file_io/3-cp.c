5ce6b05134bc6d86c96c041d56c6bf30ceb46b61
MrHelsinki <taha.chad2001@gmail.com>
2023-11-27 17:28:54 +0100

    auto push script

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+*main - main
+*@argc: arg count
+*@argv: args
+*Return: 1;
+*/
+
+int main(int argc, char **argv)
+{
+	int file_from, file_to;
+	char *f_from, *f_to;
+	int n;
+	char buffer[BUFSIZ];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	f_from = argv[1];
+	file_from = open(f_from, O_RDONLY);
+	if (!file_from)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", f_from);
+		exit(98);
+	}
+
+	f_to = argv[2];
+	file_to = open(f_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (!file_to)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", f_to);
+		exit(99);
+	}
+	while ((n = read(file_from, buffer, BUFSIZ)) > 0)
+		write(file_to, buffer, n);
+
+	close(file_from);
+	close(file_to);
+
+	return (0);
+}
