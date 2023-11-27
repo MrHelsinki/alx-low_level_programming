aa3822fe93f092037656f0b759b9d2fa514d44ce
MrHelsinki <taha.chad2001@gmail.com>
2023-11-27 16:40:41 +0100

    auto push script

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,15 +10,20 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int file;
+	int x;
 
 	if (!filename)
 		return (-1);
 
 	file = open(filename, O_WRONLY | O_APPEND);
+	if (!file)
+		return (-1);
 
 	if (text_content)
 	{
-		write(file, text_content, strlen(text_content));
+		x = write(file, text_content, strlen(text_content));
+		if (x != strlen(text_content))
+			return (-1);
 	}
 
 	close(file);
