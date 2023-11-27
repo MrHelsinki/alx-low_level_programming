85ef91df0eb029146af12e4c5ccb8ff8919a83a3
MrHelsinki <taha.chad2001@gmail.com>
2023-11-27 16:36:35 +0100

    auto push script

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*append_text_to_file - add text to the end of file
+*@filename: name of file
+*@text_content: text to append
+*Return: 1 on success and - 1 on failure
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file;
+
+	if (!filename)
+		return (-1)
+
+	file = open(filename, O_WRONLY | O_APPEND);
+
+	if (text_content)
+	{
+		write(filename, text_content, strlen(text_content));
+	}
+
+	close(file);
+	return (1);
+}
