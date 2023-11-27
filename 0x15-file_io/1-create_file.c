a2dcac9e10df94110729a0d1d8a879f0e3638555
MrHelsinki <taha.chad2001@gmail.com>
2023-11-27 16:24:15 +0100

    auto push script

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	if (!file)
 		return (-1);
 
-	if(text_content)
+	if (text_content)
 	{
 		write(file, text_content, strlen(text_content));
 	}
