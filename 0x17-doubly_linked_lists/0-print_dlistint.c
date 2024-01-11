c4cfb293635603034dbbe5d25d3a617397b8b0bf
MrHelsinki <taha.chad2001@gmail.com>
2024-01-12 00:27:21 +0100

    auto push msg

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+*print_dlistint - prints all elems of a list
+*h: head of the list
+*Return: number of elems in the list
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *current;
+	size_t count = 0;
+
+	current = h;
+
+	while (current->next != NULL)
+	{
+		printf(current->n);
+		current = h->next;
+		count++;
+	}
+
+	return (count)
+}
