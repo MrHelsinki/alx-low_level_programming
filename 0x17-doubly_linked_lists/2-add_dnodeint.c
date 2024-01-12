56a7632dedb3fa30d18d278ac613884dcc8b0232
MrHelsinki <taha.chad2001@gmail.com>
2024-01-12 01:23:06 +0100

    auto push msg

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,4 +1,4 @@
-#include "lists"
+#include "lists.h"
 
 /**
 *add_dnodeint - adds node before head
@@ -22,8 +22,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->prev = NULL;
 
 	if (*head != NULL)
-		*head->prev = new;
+		(*head)->prev = new;
 	*head = new;
 
-	return (head);
+	return (*head);
 }
