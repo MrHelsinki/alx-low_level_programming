e96fd1a6c2aa2e438db3b928aa606d004ea3fb89
MrHelsinki <taha.chad2001@gmail.com>
2024-01-12 01:14:26 +0100

    auto push msg

---
+++ 0x17-doubly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,29 @@
+#include "lists"
+
+/**
+*add_dnodeint - adds node before head
+*@head: ptr of head
+*@n: value
+*Return: head
+*/
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+
+	if (*head != NULL)
+		*head->prev = new;
+	*head = new;
+
+	return (head);
+}
